2ebba1747f1b16529522d11ee19ee4e6de0e447e
Anthony Merino <anthony.merino@icloud.com>
2025-04-14 22:29:56 -0600

    refacored navbar for cleanliness and reusablity

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -6,12 +6,33 @@ import { WalletButton, TextGenerate } from './';
 import { logo, menu, search } from '../assets';
 import { navlinks } from '../constants';
 
+interface SearchBarProps {
+  handleSearch: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchBar: React.FC<SearchBarProps> = ({ handleSearch }) => (
+  <div className='flex flex-row w-full px-4 py-2 h-[52px] bg-black-1 rounded-[100px]'>
+    <input
+      type='text'
+      placeholder='Search Campaigns'
+      className='flex w-full font-epilogue font-normal text-[14px] placeholder:text-placeholder-text text-white bg-transparent outline-hidden'
+      onChange={handleSearch}
+    />
+    <div className='w-[72px] h-full rounded-[20px] bg-purple-main flex justify-center items-center cursor-pointer'>
+      <img
+        src={search}
+        alt='search'
+        className='w-[15px] h-[15px] object-contain'
+      />
+    </div>
+  </div>
+);
+
 const Navbar = () => {
   const navigate = useNavigate();
   const { isActive, setActivePage } = useActivePageContext();
   const { setSearchTerm } = useContractContext();
   const [toggleDrawer, setToggleDrawer] = useState(false);
-
   const title = 'Civil Share';
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,36 +43,15 @@ const Navbar = () => {
     <div>
       {/* Large screen navigation */}
       <div className='hidden lg:flex w-full flex-row justify-between items-center pb-12'>
-        {/* Header */}
-        <div>
-          <h1 className='h-fit w-[125%]'>
-            <TextGenerate
-              words={title}
-              className='relative -mt-2 pb-2'
-            />
-          </h1>
+        <h1 className='h-fit w-full'>
+          <TextGenerate
+            words={title}
+            className='relative -mt-2 pb-2'
+          />
+        </h1>
+        <div className='w-[60%] pr-4'>
+          <SearchBar handleSearch={handleSearch} />
         </div>
-
-        {/* Search */}
-        <div className='w-[60%]'>
-          <div className='flex flex-row w-[65%] ml-18 px-4 py-2 h-[52px] bg-black-1 rounded-[100px]'>
-            <input
-              type='text'
-              placeholder='Search Campaigns'
-              className='flex w-full font-epilogue font-normal text-[14px] placeholder:text-placeholder-text text-white bg-transparent outline-hidden'
-              onChange={(e) => handleSearch(e)}
-            />
-            <div className='w-[72px] h-full rounded-[20px] bg-purple-main flex justify-center items-center cursor-pointer'>
-              <img
-                src={search}
-                alt='search'
-                className='w-[15px] h-[15px] object-contain'
-              />
-            </div>
-          </div>
-        </div>
-
-        {/* Button/logo */}
         <div className='flex flex-row gap-4'>
           <WalletButton />
           <Link
@@ -71,17 +71,12 @@ const Navbar = () => {
       {/* Sm and Md screen navigation */}
       <div className='hidden sm:flex lg:hidden w-full flex-col justify-between pb-12'>
         <div className='flex flex-row justify-between pb-8'>
-          {/* Header */}
-          <div>
-            <h1 className='h-fit'>
-              <TextGenerate
-                words={title}
-                className='relative -mt-2 pb-2'
-              />
-            </h1>
-          </div>
-
-          {/* Button/logo */}
+          <h1 className='h-fit'>
+            <TextGenerate
+              words={title}
+              className='relative -mt-2 pb-2'
+            />
+          </h1>
           <div className='flex flex-row gap-4'>
             <WalletButton />
             <Link
@@ -97,25 +92,7 @@ const Navbar = () => {
             </Link>
           </div>
         </div>
-
-        {/* Search */}
-        <div>
-          <div className='flex flex-row w-full px-4 md:px-6 py-2 h-[52px] bg-black-1 rounded-[100px]'>
-            <input
-              type='text'
-              placeholder='Search Campaigns'
-              className='flex w-full font-epilogue font-normal text-[14px] placeholder:text-placeholder-text text-white bg-transparent outline-hidden'
-              onChange={(e) => handleSearch(e)}
-            />
-            <div className='w-[72px] h-full rounded-[20px] bg-purple-main flex justify-center items-center cursor-pointer'>
-              <img
-                src={search}
-                alt='search'
-                className='w-[15px] h-[15px] object-contain'
-              />
-            </div>
-          </div>
-        </div>
+        <SearchBar handleSearch={handleSearch} />
       </div>
 
       {/* XSmall screen navigation */}
@@ -128,40 +105,22 @@ const Navbar = () => {
               className='w-[60%] h-[60%] object-contain'
             />
           </div>
-
           <h1>
             <TextGenerate
               words={title}
               className='pb-4'
             />
           </h1>
-
           <img
             src={menu}
             alt='menu'
-            className={'w-[34px] h-[34px] object-contain cursor-pointer'}
+            className='w-[34px] h-[34px] object-contain cursor-pointer'
             onClick={() => setToggleDrawer((prev) => !prev)}
           />
         </div>
-
         <div className='pb-8'>
-          <div className='flex flex-row w-full px-4 md:px-6 py-2 h-[52px] bg-black-1 rounded-[100px]'>
-            <input
-              type='text'
-              placeholder='Search Campaigns'
-              className='flex w-full font-epilogue font-normal text-[14px] placeholder:text-placeholder-text text-white bg-transparent outline-hidden'
-              onChange={(e) => handleSearch(e)}
-            />
-            <div className='w-[72px] h-full rounded-[20px] bg-purple-main flex justify-center items-center cursor-pointer'>
-              <img
-                src={search}
-                alt='search'
-                className='w-[15px] h-[15px] object-contain'
-              />
-            </div>
-          </div>
+          <SearchBar handleSearch={handleSearch} />
         </div>
-
         <div
           className={`absolute top-[60px] right-0 left-0 bg-black-1 z-10 shadow-secondary py-4 ${!toggleDrawer ? '-translate-y-[100vh]' : 'translate-y-0'} transition-all duration-700`}>
           <ul>
@@ -188,7 +147,6 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
-
           <div className='flex mx-4'>
             <WalletButton />
           </div>
